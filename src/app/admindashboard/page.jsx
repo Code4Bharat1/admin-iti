c2a62bf72c3b7f55ffdcc3e9d1669b2ab1005518
Pranali bobade <pranalibobade24@gmail.com>
2025-07-01 17:21:38 +0530

    admin1

--- src/app/admindashboard/page.jsx
+++ src/app/admindashboard/page.jsx
@@ -1,9 +1,11 @@
+import Sidebar from '@/components/admin/sidebar'
 import Dashboard from '@/components/admindashboard/dashboard'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
+      <Sidebar/>
         <Dashboard/>
     
         </div>
