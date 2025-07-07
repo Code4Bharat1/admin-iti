9ce799a18f1c0886d35f8e99d95df2b566b794c6
mariyask04 <skmariya484@gmail.com>
2025-07-07 12:17:58 +0530

    next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        'www.shutterstock.com',
-        'images.app.goo.gl',
-        'localhost',  // ✅ allow your local backend host
-      ],
-    },
-  };
-  
-export default  nextConfig; // ✅ Use CommonJS if using next.config.js
-  
+  images: {
+    domains: [
+      'www.shutterstock.com',
+      'images.app.goo.gl',
+      'localhost',  // ✅ allow your local backend host
+    ],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig; // ✅ Use CommonJS if using next.config.js
